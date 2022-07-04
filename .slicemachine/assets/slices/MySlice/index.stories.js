5a2affab095145e125f7c37bc04067c91c4d6bcf
fullstack0103 <fullstack0103@gmail.com>
2022-07-04 17:42:50 +0800

    Feat: next project

---
+++ .slicemachine/assets/slices/MySlice/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/MySlice';
+
+export default {
+  title: 'slices/MySlice'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","name":"Default","slice_type":"my_slice","items":[],"primary":{"title":[{"type":"heading1","text":"Incentivize killer web-readiness","spans":[]}],"description":[{"type":"paragraph","text":"Velit pariatur velit officia sunt exercitation minim ex et tempor excepteur officia id. Quis aliquip mollit exercitation sint dolor.","spans":[]}]},"id":"_Default"}} />
+_Default.storyName = 'Default'
